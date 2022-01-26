82191cb2aaf3c81cd030dc350300b803e3d80609
dotnotation <dotinmotionproductions@gmail.com>
2022-01-26 14:38:59 -0500

    commit tree 0123c75488c81516ddeee42933ff8e81fbcbf023

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+            // passed in from index.js
+        }
+    }
+    render(){
+        if (this.state.secondsLeft > 0){
+            return <h2>{this.state.secondsLeft} seconds left before I go boom!</h2>
+        } else {
+            return <h1>Boom!</h1>
+        }
+        
+    }
+}
+
+export default Bomb
